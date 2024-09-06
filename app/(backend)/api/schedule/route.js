c20f6a3224c46a8a6387cde71b60d77ee5a4dc52
JihanHayaMufialdo <jihannhayaa@gmail.com>
2024-09-07 00:01:39 +0700

    fix api schedule and schedule-rooms

--- app/(backend)/api/schedule/route.js
+++ app/(backend)/api/schedule/route.js
@@ -71,7 +71,7 @@ export async function POST(req) {
       const newSchedule = await prisma.schedule.create({
         data: {
           idScheduleDay,
-          idClassLecturer
+          idClassLecturer,
           idScheduleSession
         },
       });
