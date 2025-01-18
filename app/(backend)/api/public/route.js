6aa2046e357a477cd2b0af2081dc8bea3a6f7c53
JihanHayaMufialdo <jihannhayaa@gmail.com>
2025-01-18 14:03:33 +0700

    Memperbarui data D3

--- app/(backend)/api/public/route.js
+++ app/(backend)/api/public/route.js
@@ -120,7 +120,7 @@ export async function GET(request) {
                   },
                   subjectType: true,
                 },
-              },
+              }
             },
           },
         },
